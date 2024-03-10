f4ff063dc78f303dfadca997c420c2af94eafbca
Irine Ann Jikku <irineannjikku@gmail.com>
2024-03-10 07:56:32 +0530

    first page completed

---
+++ index.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZjLNNuIulevmMs6xw_XkJ4KZrOzOfjdc",
+    authDomain: "my-aqucasense1.firebaseapp.com",
+    projectId: "my-aqucasense1",
+    storageBucket: "my-aqucasense1.appspot.com",
+    messagingSenderId: "1023900740203",
+    appId: "1:1023900740203:web:a154e822c1d16092fd5f2e"
+  };
+  
